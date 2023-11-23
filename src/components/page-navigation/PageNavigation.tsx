cd0f1e9054f79d9ca841d54da4b8df596f6ff1df
hativat.hamarom <hativat.hamarom@gmail.com>
2023-11-23 12:01:28 +0200

    change ws title ,fix css responsive

--- src/components/page-navigation/PageNavigation.tsx
+++ src/components/page-navigation/PageNavigation.tsx
@@ -4,6 +4,7 @@ import { PageMenuProps } from "../../types/page-menu"
 import { RxHamburgerMenu } from "react-icons/rx"
 import { useContext } from "react"
 import { SideMenuContext } from "../../contexts/SideMenuContext"
+import { v4 } from "uuid"
 
 export default function PageNavigation(props: PageMenuProps) {
 
@@ -15,18 +16,23 @@ export default function PageNavigation(props: PageMenuProps) {
                 <span>
                     <RxHamburgerMenu
                         size={'30px'}
-                        onClick={()=>{sideMenuContext?.setIsOpen(true)}}
+                        onClick={() => { sideMenuContext?.setIsOpen(true) }}
                     />
                 </span>
             </div>
-            <div>
-            {
-                props.items.map(item => {
-                    return (
-                        <NavLink to={item.to}> {item.text} </NavLink>
-                    )
-                })
-            }
+            <div className={style["side-menu-items"]}>
+                {
+                    props.items.map(item => {
+                        return (
+                            <NavLink
+                                key={v4()}
+                                to={item.to}
+                            >
+                                {item.text}
+                            </NavLink>
+                        )
+                    })
+                }
             </div>
             <div>
 
--- src/components/video/video-card-list/VideoCardList.tsx
+++ src/components/video/video-card-list/VideoCardList.tsx
@@ -1,7 +1,7 @@
 import { VideoCardListProps } from "../../../types/video";
 import VideoCard from "../video-card/VideoCard";
 import style from "./VideoCardList.module.css"
-
+import { v4 } from "uuid"
 export default function VideoList(props: VideoCardListProps) {
 
     return (
@@ -11,6 +11,7 @@ export default function VideoList(props: VideoCardListProps) {
                 props.videos.map(item => {
                     return (
                         <VideoCard 
+                            key={v4()}
                             title={item.title} 
                             src={item.src}                            
                         />
