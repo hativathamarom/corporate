cd3fc04333f6c3041d4146a38e674d5dcc278b94
hativat.hamarom <hativat.hamarom@gmail.com>
2023-12-01 13:59:11 +0200

    image slider

--- src/components/image-slider/ImageSlider.tsx
+++ src/components/image-slider/ImageSlider.tsx
@@ -2,6 +2,7 @@
 import { useEffect, useRef, useState } from "react"
 import style from "./ImageSlider.module.css"
 import { ImageSliderProps } from "../../types/image-slider"
+import { MdOutlineArrowBackIos, MdOutlineArrowForwardIos } from "react-icons/md";
 
 export default function ImageSlider(props: ImageSliderProps) {
 
@@ -32,30 +33,28 @@ export default function ImageSlider(props: ImageSliderProps) {
     }
 
     return (
-            <div className={style["images"]}>
-                <div>
-                    <img
-                        className={`${currentImage % 2 === 0 ? style["image-animation1"] : style["image-animation2"]}`}
-                        ref={imageRef}
-                        src={props.images[currentImage]}
-                    />
+        <div className={style["images"]}>
+            <img
+                className={`${currentImage % 2 === 0 ? style["image-animation1"] : style["image-animation2"]}`}
+                ref={imageRef}
+                src={props.images[currentImage]}
+            />
+            <div className={style["image-switch"]}>
+
+                <div className={style["image-switch-btn"]}>
+                    <button onClick={onNextHandler}> <MdOutlineArrowForwardIos size={"2rem"} /> </button>
                 </div>
-                <div className={style["image-switch"]}>
 
-                    <div className={style["image-switch-btn"]}>
-                        <button onClick={onNextHandler}> {`<`} </button>
-                    </div>
+                {/* <div className={`${style["image-ball"]} ${currentImage === 4 && style["image-ball-active"]}`}></div>
+                <div className={`${style["image-ball"]} ${currentImage === 3 && style["image-ball-active"]}`}></div>
+                <div className={`${style["image-ball"]} ${currentImage === 2 && style["image-ball-active"]}`}></div>
+                <div className={`${style["image-ball"]} ${currentImage === 1 && style["image-ball-active"]}`}></div>
+                <div className={`${style["image-ball"]} ${currentImage === 0 && style["image-ball-active"]}`}></div> */}
 
-                    <div className={`${style["image-ball"]} ${currentImage === 4 && style["image-ball-active"]}`}></div>
-                    <div className={`${style["image-ball"]} ${currentImage === 3 && style["image-ball-active"]}`}></div>
-                    <div className={`${style["image-ball"]} ${currentImage === 2 && style["image-ball-active"]}`}></div>
-                    <div className={`${style["image-ball"]} ${currentImage === 1 && style["image-ball-active"]}`}></div>
-                    <div className={`${style["image-ball"]} ${currentImage === 0 && style["image-ball-active"]}`}></div>
-
-                    <div className={style["image-switch-btn"]}>
-                        <button onClick={onPrevHandler}> {`>`} </button>
-                    </div>
+                <div className={style["image-switch-btn"]}>
+                    <button onClick={onPrevHandler}> <MdOutlineArrowBackIos size={"2rem"}/> </button>
                 </div>
             </div>
+        </div>
     )
 }
