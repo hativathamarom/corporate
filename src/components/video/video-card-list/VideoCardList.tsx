2d28066d6c3e97fa1e69e5be63a13c6494ad631e
hativat.hamarom <hativat.hamarom@gmail.com>
2023-12-02 22:16:07 +0200

    set new css rule

--- src/components/video/video-card-list/VideoCardList.tsx
+++ src/components/video/video-card-list/VideoCardList.tsx
@@ -5,7 +5,7 @@ import { v4 } from "uuid"
 export default function VideoList(props: VideoCardListProps) {
 
     return (
-        <div className={style["container"]}>
+        <div className={style["video-container"]}>
             {
                 props.videos.length!== 0 ?
                 props.videos.map(item => {
