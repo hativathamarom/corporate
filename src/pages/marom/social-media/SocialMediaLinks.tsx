6526277fd3d00a6c6cd5f7e8d26a490c53143f5a
hativat.hamarom <hativat.hamarom@gmail.com>
2023-12-21 12:56:56 +0200

    fix yizkor

--- src/pages/marom/social-media/SocialMediaLinks.tsx
+++ src/pages/marom/social-media/SocialMediaLinks.tsx
@@ -3,14 +3,16 @@ import style from "./SocialMediaLinks.module.css"
 export default function SocialMediaLinks() {
     return (
         <ul className={style["social-list"]}>
-            <li className={`${style["icon"]} ${style["youtube"]}`}>
-                <span className={style["tooltip"]}>YouTube</span>
-                <span>
-                    <svg xmlns="http://www.w3.org/2000/svg" height="16" width="18" viewBox="0 0 576 512">
-                        <path d="M549.7 124.1c-6.3-23.7-24.8-42.3-48.3-48.6C458.8 64 288 64 288 64S117.2 64 74.6 75.5c-23.5 6.3-42 24.9-48.3 48.6-11.4 42.9-11.4 132.3-11.4 132.3s0 89.4 11.4 132.3c6.3 23.7 24.8 41.5 48.3 47.8C117.2 448 288 448 288 448s170.8 0 213.4-11.5c23.5-6.3 42-24.2 48.3-47.8 11.4-42.9 11.4-132.3 11.4-132.3s0-89.4-11.4-132.3zm-317.5 213.5V175.2l142.7 81.2-142.7 81.2z" />
-                    </svg>
-                </span>
-            </li>
+            <a href="https://www.youtube.com/channel/UC0k37K40HJSHrW9PWNSUWKw" target="_blank">
+                <li className={`${style["icon"]} ${style["youtube"]}`}>
+                    <span className={style["tooltip"]}>YouTube</span>
+                    <span>
+                        <svg xmlns="http://www.w3.org/2000/svg" height="16" width="18" viewBox="0 0 576 512">
+                            <path d="M549.7 124.1c-6.3-23.7-24.8-42.3-48.3-48.6C458.8 64 288 64 288 64S117.2 64 74.6 75.5c-23.5 6.3-42 24.9-48.3 48.6-11.4 42.9-11.4 132.3-11.4 132.3s0 89.4 11.4 132.3c6.3 23.7 24.8 41.5 48.3 47.8C117.2 448 288 448 288 448s170.8 0 213.4-11.5c23.5-6.3 42-24.2 48.3-47.8 11.4-42.9 11.4-132.3 11.4-132.3s0-89.4-11.4-132.3zm-317.5 213.5V175.2l142.7 81.2-142.7 81.2z" />
+                        </svg>
+                    </span>
+                </li>
+            </a>
             <li className={`${style["icon"]} ${style["instagram"]}`}>
                 <span className={style["tooltip"]}>Instagram</span>
                 <span>
