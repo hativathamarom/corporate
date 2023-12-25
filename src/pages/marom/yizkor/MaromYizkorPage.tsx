1b6ee33e979c844ea28002999f87a57483647847
hativat.hamarom <hativat.hamarom@gmail.com>
2023-12-25 19:03:56 +0200

    remove unnessery imports

--- src/pages/marom/yizkor/MaromYizkorPage.tsx
+++ src/pages/marom/yizkor/MaromYizkorPage.tsx
@@ -3,7 +3,6 @@ import style from "./MaromYizkorPage.module.css"
 import YizkorCardList from "../../../components/yizkor/yizkor-card-list/YizkorCardList"
 import { maromYizkorListData } from "./maromYizkorListData"
 import PageNavigation from "../../../components/page-navigation/PageNavigation"
-import { BiArrowBack } from "react-icons/bi";
 
 export default function MaromYizkorPage() {
     return (
