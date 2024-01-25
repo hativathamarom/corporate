fb69a1d28a897b34d263be34f1a03e61660892f3
hativat.hamarom <hativat.hamarom@gmail.com>
2024-01-25 12:34:13 +0200

    add more images

--- src/pages/marom/slide-images.ts
+++ src/pages/marom/slide-images.ts
@@ -22,6 +22,8 @@ import image21 from "../../assets/home/images/21.jpg"
 import image22 from "../../assets/home/images/22.jpg"
 import image23 from "../../assets/home/images/23.jpeg"
 import image24 from "../../assets/home/images/24.jpg"
+import image25 from "../../assets/home/images/25.jpg"
+import image26 from "../../assets/home/images/26.jpg"
 
 export const images = [
     image1,
@@ -48,4 +50,6 @@ export const images = [
     image22,
     image23,
     image24,
+    image25,
+    image26
 ]
